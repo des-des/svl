b1aa1d9904bb7e61a49d834528d6516a0286d314
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-04-29 15:47:50 +0000

    introduce json transforms

--- index.js
+++ index.js
@@ -1,115 +1,53 @@
-const fs = require('fs')
-const path = require('path')
-const css = require('css')
-
-// (o => str => o) => o => o
-const reduceKeys = f => o => Object.keys(o).reduce(f, Object.create(null))
-
-// (str => o => { str, a }) => o => o
-const mapKeys = f => o => reduceKeys((acc, k) => {
-  const { key, value } = f(k)(o)
-  // console.log({value});
-  return setMut(key, value, acc)
-})(o)
-
-const isSingleClassSelector = selector =>
-  selector.length === 1 && selector[0][0] === '.'
-
-fL = x => { console.log(x); return x }
-fLL = label => x => {console.log(label); console.log(x); return x }
-
-const decsToStyleNoTrim = decs => (decs
-  .filter(dec => dec.type === 'declaration' || dec.length)
-  .reduce((style, dec) => dec.length ? `${style}${decsToStyle(dec)}` : `${style}${dec.property}: ${dec.value}; `, '')
-)
-
-const decsToStyle = decs => (decs
-  .filter(dec => dec.type === 'declaration' || dec.length)
-  .reduce((style, dec) => dec.length ? `${style}${decsToStyleNoTrim(dec)}` : `${style}${dec.property}: ${dec.value}; `, '')
-  .trim()
-)
-const setMut = (k, v, o) => { o[k] = v; return o }
-
-const cssToTokens = rawCss => css.parse(rawCss).stylesheet.rules
-  .filter(rule => rule.type == 'rule')
-  .filter(rule => isSingleClassSelector(rule.selectors))
-  .map(rule => ({
-    name: rule.selectors[0].slice(1),
-    declarations: rule.declarations
-  }))
-  .reduce((cssMap, rule) =>
-    setMut(rule.name, rule.declarations, cssMap),
-    Object.create(null))
+const { MERGE, ARRAY_TOGGLE, transform } = require('./lib/transform.js')
+const { eachKey, mapKeys } = require('./lib/helpers.js')
+const { tachyons } = require('./lib/css.js')
 
 const tag = name => style => innerHtml =>
   `<${name} style="${style}">${innerHtml}</${name}>`
 
-// str => o => o
-const toggleStyle = toggle => styles => {
-  console.log({styles});
-  const { found, newStyles } = styles.reduce(({ newStyles, found }, style) => {
-    if (style === toggle) {
-      return { found: true, newStyles }
+const styleToggleSpec = key => ({
+  op: MERGE,
+  spec: {
+    style: {
+      op: ARRAY_TOGGLE,
+      spec: key
     }
-    return { found, newStyles: newStyles.concat(style) }
-  }, { found: false, newStyles: []})
-
-  return found ? newStyles : newStyles.concat(toggle)
-}
-//
-// mapKeys(k => o => ({
-//   key: k,
-//   value: k === toggleKey ? !o[k] : o[k]
-// }))
+  }
+})
 
 const initComponent = engine => {
   const component = config => {
-    const component = initComponent(engine)
     console.log(config);
-    const createStyleToggler = key => styles => ({
-      key: key,
-      value: () => component(Object.assign({}, config, {style: toggleStyle(key)(config.style) }))
-    })
+    const styleTogglers = mapKeys(
+      (key, value) => ({
+        key,
+        value: () => component(transform(styleToggleSpec(key), config))
+      }),
+      engine.style
+    )
 
-    const styleTogglers = mapKeys(createStyleToggler)(engine.style)
+    const styleStr = config.style
+      .reduce(
+        (styleStr, className) => styleStr + engine.style[className],
+        ''
+      )
+      .trim()
 
-    // console.log(styleT);
+      const renderChildren = children =>
+        children.map(child => child.render ? child.render() : child)
 
-    const styleStr = decsToStyle(config.style.map(className =>
-        engine.style[className]))
-
-    const render = () => (tag
-      (config.tag)
-      (styleStr)
-      (config.children.map(child => child.render ? child.render() : child)))
+    const render = () =>
+      tag(config.tag)(styleStr)(renderChildren(config.children))
 
     return { render, sty: styleTogglers }
   }
 
+  const styleToggler = (key, config) =>
+    component(transform(styleToggleSpec(key)), config)
+
   return component
 }
 
-const go = config => {
-  const cssTokens = cssToTokens(config.style)
-
-  return initComponent({ style: cssTokens })
-  // const div = component({ tag: 'div', style: ['fl', 'w-100', 'w-50'], children: [] })
-  // console.log(div.render());
-}
-
-const tachyonsPath = path.join(
-  __dirname,
-  'node_modules',
-  'tachyons',
-  'css',
-  'tachyons.css'
-)
-
-const tachyons = fs.readFileSync(tachyonsPath, 'utf8')
-
-go({ style: tachyons })
-
 module.exports = {
-  decsToStyle,
-  svl: go({ style: tachyons })
+  svl: initComponent({ style: tachyons })
 }
---
+++ test/transform.js
@@ -0,0 +1,101 @@
+const test = require('tape')
+
+const { transform, MERGE, ARRAY_TOGGLE } = require('../lib/transform.js')
+
+test('transform', t => {
+  t.deepEqual(
+    transform({}, { k: 'v' }),
+    { k: 'v' },
+    'emtpy transform does nothing'
+  )
+
+  t.end()
+})
+
+test('transform MERGE', t => {
+  t.deepEqual(
+    transform(
+      { op: MERGE, spec: { k: 'v' } },
+      {}
+    ),
+    { k: 'v' },
+    'simple merge sets key on empty object'
+  )
+
+  t.deepEqual(
+    transform(
+      { op: MERGE, spec: { k1: { k2: 'v' } } },
+      {}
+    ),
+    { k1: { k2: 'v' } },
+    'deeper merge sets key on empty object'
+  )
+
+  t.deepEqual(
+    transform(
+      { op: MERGE, spec: { k2: 'v2' } },
+      { k1: 'v1' }
+    ),
+    { k1: 'v1', k2: 'v2' },
+    'simple merge sets key on populated object'
+  )
+
+  t.deepEqual(
+    transform(
+      { op: MERGE, spec: { k1: { k2: 'v' } } },
+      { k1: { k3: 'v2' } }
+    ),
+    { k1: { k2: 'v', k3: 'v2' } },
+    'deeper merge sets key on populated object'
+  )
+
+  t.end()
+})
+
+test('transform ARRAY_TOGGLE', t => {
+  t.deepEqual(
+    transform(
+      { op: ARRAY_TOGGLE, spec: 'eoin' },
+      ['eoin']
+    ),
+    [],
+    'can toggle away single element'
+  )
+
+  t.deepEqual(
+    transform(
+      { op: ARRAY_TOGGLE, spec: 'des' },
+      ['des', 'des']
+    ),
+    [],
+    'can toggle away multiple elements'
+  )
+
+  t.deepEqual(
+    transform(
+      { op: ARRAY_TOGGLE, spec: 'des' },
+      []
+    ),
+    ['des'],
+    'missing element is added'
+  )
+
+  t.end()
+})
+
+test('transform MERGE with nested ARRAY_TOGGLE', t => {
+
+  t.deepEqual(
+    transform(
+      {
+        op: MERGE,
+        spec: { k1: { op: ARRAY_TOGGLE, spec: 'des' } }
+      },
+      { k1: ['des'] }
+    ),
+    { k1: [] },
+    'deeper merge sets key on populated object'
+  )
+
+  t.end()
+})
